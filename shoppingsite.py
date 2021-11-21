dc518b1c2044b5771a012dbb44c87185e2cc1023
Lauren <laurencaroleen@gmail.com>
2021-11-21 01:30:07 +0000

    Partially completed shopping site exercise?

--- shoppingsite.py
+++ shoppingsite.py
@@ -6,7 +6,7 @@ put melons in a shopping cart.
 Authors: Joel Burton, Christian Fernandez, Meggie Mahnken, Katie Byers.
 """
 
-from flask import Flask, render_template, redirect, flash
+from flask import Flask, render_template, redirect, flash, session
 import jinja2
 
 import melons
@@ -14,7 +14,7 @@ import melons
 app = Flask(__name__)
 
 # A secret key is needed to use Flask sessioning features
-app.secret_key = 'this-should-be-something-unguessable'
+app.secret_key = '6tr4oy8grhvzdisueiwa7fff5R76OTGIUBVGHrd578FDSETR68T7yioiugyfTDRTGFUYgir5IR67iuyf5'
 
 # Normally, if you refer to an undefined variable in a Jinja template,
 # Jinja silently ignores this. This makes debugging difficult, so we'll
@@ -39,8 +39,7 @@ def list_melons():
     """Return page showing all the melons ubermelon has to offer"""
 
     melon_list = melons.get_all()
-    return render_template("all_melons.html",
-                           melon_list=melon_list)
+    return render_template("all_melons.html", melon_list=melon_list)
 
 
 @app.route("/melon/<melon_id>")
@@ -50,17 +49,34 @@ def show_melon(melon_id):
     Show all info about a melon. Also, provide a button to buy that melon.
     """
 
-    melon = melons.get_by_id("meli")
+    melon = melons.get_by_id(melon_id)
     print(melon)
-    return render_template("melon_details.html",
-                           display_melon=melon)
+    return render_template("melon_details.html", display_melon=melon)
 
 
 @app.route("/cart")
-def show_shopping_cart():
+def show_shopping_cart(melon_id = "cren"):
     """Display content of shopping cart."""
+   
+    melon = melons.get_by_id(melon_id)
 
     # TODO: Display the contents of the shopping cart.
+    session['cart'] = {}
+
+    if session['cart']:
+        
+        session['cart'] = session.get(melon, 0) + 1
+        flash('The melon was successfully added')
+        print(session['cart'])
+
+        # letter_counts = {'a': 6, 'b': 7, 'e': 5}
+        #  for letter in phrase:
+        # letter_counts[letter] = letter_counts.get(letter, 0) + 1
+
+    else:
+        session['cart'] = {}
+        flash('The cart was successfully created')
+        print(session['cart'])
 
     # The logic here will be something like:
     #
@@ -78,6 +94,7 @@ def show_shopping_cart():
     # Make sure your function can also handle the case wherein no cart has
     # been added to the session
 
+    #return redirect("/cart")
     return render_template("cart.html")
 
 
@@ -92,7 +109,28 @@ def add_to_cart(melon_id):
     # TODO: Finish shopping cart functionality
 
     # The logic here should be something like:
-    #
+    melon_object = melons.get_by_id(melon_id)
+
+    # TODO: Display the contents of the shopping cart.
+    session['cart'] = {}
+
+    if session['cart']:
+        cart_list = []
+        session['cart'] = session.get(melon_id, 0) + 1
+            for melon_id in session['cart']:
+                cart_list.append(melon_object)
+                print(cart_list)
+        flash('The melon was successfully added')
+        print(session['cart'])
+
+        # letter_counts = {'a': 6, 'b': 7, 'e': 5}
+        #  for letter in phrase:
+        # letter_counts[letter] = letter_counts.get(letter, 0) + 1
+
+    else:
+        session['cart'] = {}
+        flash('The cart was successfully created')
+        print(session['cart'])
     # - check if a "cart" exists in the session, and create one (an empty
     #   dictionary keyed to the string "cart") if not
     # - check if the desired melon id is the cart, and if not, put it in
@@ -100,8 +138,7 @@ def add_to_cart(melon_id):
     # - flash a success message
     # - redirect the user to the cart page
 
-    return "Oops! This needs to be implemented!"
-
+    return render_template("cart.html", session['cart'], cart_list, cart_melon = melon_object)
 
 @app.route("/login", methods=["GET"])
 def show_login():
